b7a7b4dafc3d678f25e0255deab8daa9cbbb30ef
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 17:17:45 -0400

    Change response to html

--- server.rb
+++ server.rb
@@ -17,7 +17,17 @@ loop do                                             # Server runs forever
 
   header = "HTTP/1.1 200 OK"
   time = Time.now.ctime
-  response = "#{header}\r\n\r\n#{time}"       # Output the HTTP header with current time to the client
+  response = "
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>My first web server</title>
+  </head>
+  <body>
+      <h1>My First Web Server</h1>
+      <p>Oh hey, this is the first HTML response!</p>
+  </body>
+  </html>"       # Output the HTTP header with current time to the client
 
   client.puts(response)
 
